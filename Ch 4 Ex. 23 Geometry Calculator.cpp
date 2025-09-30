2a9a0b6422789116b24be2b474ac84fb0bbac6fc
phili <phili@STERLING-LAPTOP>
2025-09-30 14:42:49 -0400

    Add project files.

---
+++ Ch 4 Ex. 23 Geometry Calculator.cpp
@@ -0,0 +1,27 @@
+/* File Name: Chapter 4 - Geometry Calculator 
+* Programmer: Philip Rodgers
+* Date: September 2025
+* Requirements: 
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int choice;
+
+	{
+		cout << "Geometry Calculator" << endl;
+		cout << setw(10) << "1. Calculate the Area of a Circle" << endl;
+		cout << setw(10) << "2. Calculate the Area of a Rectangle" << endl;
+		cout << setw(10) << "3. Calculate the Area of a Triangle" << endl;
+		cout << setw (10) << "4. Quit" << endl;
+		cout << setw(10) << "Enter you choice (1-4):" << endl;
+	}
+
+
+} 
+
+
