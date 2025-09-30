9fe2297c082e67ce6e5e70e7b7aa8f6b6f3d8210
phili <sterlingrodgers2007@gmail.com>
2025-09-30 15:01:56 -0400

    changes

--- Ch 4 Ex. 23 Geometry Calculator.cpp
+++ Ch 4 Ex. 23 Geometry Calculator.cpp
@@ -14,11 +14,11 @@ int main()
 
 	{
 		cout << "Geometry Calculator" << endl;
-		cout << setw(10) << "1. Calculate the Area of a Circle" << endl;
-		cout << setw(10) << "2. Calculate the Area of a Rectangle" << endl;
-		cout << setw(10) << "3. Calculate the Area of a Triangle" << endl;
+		cout << setw(36) << "1. Calculate the Area of a Circle" << endl;
+		cout << setw(39) << "2. Calculate the Area of a Rectangle" << endl;
+		cout << setw (38) << "3. Calculate the Area of a Triangle" << endl;
 		cout << setw (10) << "4. Quit" << endl;
-		cout << setw(10) << "Enter you choice (1-4):" << endl;
+		cout << setw(26) << "Enter you choice (1-4):" << endl;
 	}
 
 
